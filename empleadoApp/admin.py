5082c3d1f09c1d9134736012eb6b88d08a42c5f3
Cristofher <Cristofher.rojas@outlook.com>
2025-10-13 11:23:36 -0300

    Admin actualizado

--- empleadoApp/admin.py
+++ empleadoApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from empleadoApp.models import Empleado
 
 # Register your models here.
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ['id','nombre','email']
+    
+    
+# Register your models here.
+admin.site.register(Empleado, EmpleadoAdmin)
